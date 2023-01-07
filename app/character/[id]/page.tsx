4443ad145321fddceba83df582df754debc65a74
Ismael <ismael.csr@gmail.com>
2023-01-06 21:29:00 -0300

    Added generateStaticParams in locations for prebuild

--- app/character/[id]/page.tsx
+++ app/character/[id]/page.tsx
@@ -1,15 +1,36 @@
+import Image from "next/image";
+import { ICharacter } from "../../interfaces";
+import styles from "../CardCharacter.module.css";
 
 
-interface myParams {    
-    id: string;    
+
+interface myParams {
+    id: string;
 }
 
-export default function CharacterPage({params}: {params: myParams}) {
-    const { id } = params;
+const getCharacter = async (id: number) => {
+    const res = await fetch('https://rickandmortyapi.com/api/character/' + id);
+    const data: ICharacter = await res.json();
+    return data;
+}
 
+export default async function CharacterPage({ params }: { params: myParams }) {
+    const { id } = params;
+    const character = await getCharacter(parseInt(id));
     return (
-        <div>
-            Character: {id}
+        <div className={styles.container}>
+             <h5>Character #{id}</h5>
+            <Image
+                src={character.image}
+                alt="Character"
+                width={150}
+                height={150}
+                priority
+            />
+            <h1>{character.name}</h1>
+            <hr />
+            <h5>Specie</h5>
+            <h2>{character.species}</h2>
         </div>
     );
 }

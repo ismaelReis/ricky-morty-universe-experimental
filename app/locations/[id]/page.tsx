7ae5ac9aa733ad102bb2ba9e95937e1203ee559e
Ismael <ismael.csr@gmail.com>
2023-01-06 21:28:13 -0300

    Caching data and prebuild for locations done

--- app/locations/[id]/page.tsx
+++ app/locations/[id]/page.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
-import { ILocation,ICharacter } from "../../interfaces";
+import { ILocation, ICharacter } from "../../interfaces";
 import CardCharacter from "../../character/CardCharacter";
 import styles from '../CardLocation.module.css';
 
 interface myParams {
-    id: string;
+    location: ILocation,
+    characters: ICharacter[],
 }
 
+const getLocations = async () => {
+    const res = await fetch('https://rickandmortyapi.com/api/location');
+    const data = await res.json();
+    return data.results;
+}
 
 const getLocation = async (id: number) => {
-    const res = await fetch('https://rickandmortyapi.com/api/location/' + id);
+    const res = await fetch('https://rickandmortyapi.com/api/location/' + id, { next: { revalidate: 60 } });
     const data: ILocation = await res.json();
     return data;
 }
@@ -26,14 +32,24 @@ const reduceResidents = (residents: ICharacter[]) => {
     const randomResidents = residents.sort(() => Math.random() - 0.5).slice(0, (residents.length > 5 ? 5 : residents.length));
     return randomResidents;
 }
-export default async function Location({params}: {params: myParams}) {
+
+export async function generateStaticParams() {
+    var locations = await getLocations();
+
+    return locations.map((_location: ILocation) => ({
+        id: _location.id.toString(),
+    }));
+}
+
+
+export default async function Location({ params }: { params: { id: string } }) {
     const { id } = params;
     const location = await getLocation(parseInt(id));
     const residents = await getResidentes(location.residents);
     const reducedResidents = reduceResidents(residents);
     return (
         <div className={styles.container}>
-            <h5>Location #{id}</h5>
+            <h5>Location #{location.id}</h5>
             <h1>{location.name}</h1>
             <hr />
             <h5>Type</h5>
@@ -44,8 +60,8 @@ export default async function Location({params}: {params: myParams}) {
             <hr />
             <div className={styles.cards}>
                 {
-                    residents.length <= 0 ? "No residents found" :
-                    reducedResidents.map((resident, i) => (
+                    reducedResidents.length <= 0 ? "No residents found" :
+                        reducedResidents.map((resident, i) => (
                             <Link key={i} href={"/character/" + resident.id} style={{ textDecoration: 'none' }}>
                                 <CardCharacter character={resident} />
                             </Link>
